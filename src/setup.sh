e412782aa135a452894b0ab1f0cc4e5df037a0d8
Jie He <sj-jie@sjtu.edu.cn>
2018-01-11 16:27:15 +0800

    Update setup.sh

--- src/setup.sh
+++ src/setup.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 # Setup for the whole project
+set -e 
 
 if [ "$(id -u)" != "0" ]; then
    echo "Please run as root" 1>&2
@@ -7,38 +8,11 @@ if [ "$(id -u)" != "0" ]; then
 fi
 
 #python-dev 
-
-if [ $? -eq 0 ]; then
-	sudo apt-get install python-pip python-numpy python-scipy python-matplotlib software-properties-common zlib1g-dev
-	if [ $? -eq 0 ]; then
-		sudo pip install -U scikit-learn	
-		if [ $? -eq 0 ]; then
-			sudo pip install --upgrade gensim
-			if [ $? -eq 0 ]; then
-				add-apt-repository ppa:george-edison55/cmake-3.x
-				sudo apt-get install cmake
-				if [ $? -eq 0 ]; then
-					sudo pip install -U nltk
-					if [ $? -eq 0 ]; then
-						export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.10.0rc0-cp27-none-linux_x86_64.whl
-						sudo pip install --upgrade $TF_BINARY_URL
-					fi
-				fi
-			fi
-		fi
-	fi	
-fi
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+sudo apt-get install python-pip python-numpy python-scipy python-matplotlib software-properties-common zlib1g-dev
+sudo pip install -U scikit-learn	
+sudo pip install --upgrade gensim
+add-apt-repository ppa:george-edison55/cmake-3.x
+sudo apt-get install cmake
+sudo pip install -U nltk
+export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.10.0rc0-cp27-none-linux_x86_64.whl
+sudo pip install --upgrade $TF_BINARY_URL
